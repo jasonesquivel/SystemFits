080be194af57514b7d20d6d4873b0c9648e766f3
jasonesquivel <95312146+jasonesquivel@users.noreply.github.com>
2023-05-03 02:02:33 -0600

    PopUpMenuCaalorias

--- app/src/main/java/ni/edu/uca/systemfits/Comida.kt
+++ app/src/main/java/ni/edu/uca/systemfits/Comida.kt
@@ -5,12 +5,14 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
+import ni.edu.uca.systemfits.databinding.FragmentComidaBinding
 
 
 class Comida : Fragment() {
 
-
-    override fun onCreate(savedInstanceState: Bundle?) {
+    private lateinit var binding:FragmentComidaBinding
+            override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
 
@@ -21,9 +23,22 @@ class Comida : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_comida, container, false)
+        binding= FragmentComidaBinding.inflate(inflater, container, false)
+
+        return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
 
+
+        super.onViewCreated(view, savedInstanceState)
+
+
+        binding.btnEditarComida.setOnClickListener {
+            val showPopUpCalorias =PopUpCalorias()
+            showPopUpCalorias.show((activity as AppCompatActivity).supportFragmentManager, "showPopUp")
+        }
+
+
+    }
 }
