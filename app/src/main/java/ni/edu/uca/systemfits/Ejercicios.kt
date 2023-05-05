3415e2c1f67f24d0ebbc589ac27e6e34ff3f00a1
jasonesquivel <95312146+jasonesquivel@users.noreply.github.com>
2023-05-05 02:42:03 -0600

    Se agrego el disenio de ejercicios y la edicion de ejercicios

--- app/src/main/java/ni/edu/uca/systemfits/Ejercicios.kt
+++ app/src/main/java/ni/edu/uca/systemfits/Ejercicios.kt
@@ -1,29 +1,69 @@
 package ni.edu.uca.systemfits
 
+import android.annotation.SuppressLint
+import android.content.Context
 import android.os.Bundle
+import android.view.Gravity
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.PopupWindow
+import ni.edu.uca.systemfits.databinding.FragmentEjerciciosBinding
+
 
 
 class Ejercicios : Fragment() {
 
-
+private lateinit var binding: FragmentEjerciciosBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
 
         }
     }
+    //ESTE ES PARA EL AGREGAR MEDIDA POPUP//
+    private fun showCustomPopupAgregarMedida() {
+        val inflater =
+            requireContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+        val popupView = inflater.inflate(R.layout.fragment_dialog_input_agregar_ejercicio, null)
 
+        val popupWindow = PopupWindow(
+            popupView,
+            ViewGroup.LayoutParams.MATCH_PARENT,
+            ViewGroup.LayoutParams.MATCH_PARENT
+        )
+
+        val closeButton = popupView.findViewById<ImageView>(R.id.close_popup_button)
+        closeButton.setOnClickListener {
+            popupWindow.dismiss()
+        }
+
+        popupWindow.isOutsideTouchable = true
+        popupWindow.isFocusable = true
+
+        popupWindow.showAtLocation(binding.root, Gravity.CENTER, 0, 0)
+    }
+    @SuppressLint("SuspiciousIndentation")
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_ejercicios, container, false)
+    binding=FragmentEjerciciosBinding.inflate(inflater, container, false)
+        return binding.root
+
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val showPopupButton = binding.btnAgregarEjercicio
+        showPopupButton.setOnClickListener {
+            showCustomPopupAgregarMedida()
+        }
+
     }
 
 
 }
+
