5d851a95b59fc0b28d762fda0e16beb278d35cb5
JanCarlosPM <jan.prado13373@est.uca.edu.ni>
2023-05-14 13:26:55 -0600

    Cambios en los diseños de botones y pop up de agregar, editar y eliminar

--- app/src/main/java/ni/edu/uca/systemfits/vistas/MenuPrincipal.kt
+++ app/src/main/java/ni/edu/uca/systemfits/vistas/MenuPrincipal.kt
@@ -1,11 +1,12 @@
 package ni.edu.uca.systemfits.vistas
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import ni.edu.uca.systemfits.databinding.FragmentMenuPrincipalBinding
+
 
 class MenuPrincipal : Fragment() {
     private lateinit var binding: FragmentMenuPrincipalBinding
