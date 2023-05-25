b1d8c9c15d42d09233d5527793cbb7799b8a103d
LeoncioPavon <leoncio.pavon14107@est.uca.edu.ni>
2023-05-25 03:07:54 -0600

    Menu Principal funcional y correciones minimas

---
+++ app/src/main/java/ni/edu/uca/systemfits/data/database/dao/adapter/MenuPrincipalAdapter.kt
@@ -0,0 +1,156 @@
+package ni.edu.uca.systemfits.data.database.dao.adapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import ni.edu.uca.systemfits.data.database.entities.Comidas
+import ni.edu.uca.systemfits.data.database.entities.Ejercicios
+import ni.edu.uca.systemfits.data.database.entities.Medidas
+import ni.edu.uca.systemfits.databinding.ItemComidasBinding
+import ni.edu.uca.systemfits.databinding.ItemEjerciciosBinding
+import ni.edu.uca.systemfits.databinding.ItemMedidasBinding
+
+class MenuPrincipalAdapter(
+    private var comidas: List<Comidas>,
+    private var medidas: List<Medidas>,
+    private var ejercicios: List<Ejercicios>,
+    private var onItemClick: (Any) -> Unit
+) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+
+    fun updateData(
+        newComidas: List<Comidas>,
+        newMedidas: List<Medidas>,
+        newEjercicios: List<Ejercicios>
+    ) {
+        comidas = newComidas
+        medidas = newMedidas
+        ejercicios = newEjercicios
+        notifyDataSetChanged()
+    }
+
+    companion object {
+        private const val ITEM_TYPE_COMIDA = 0
+        private const val ITEM_TYPE_MEDIDA = 1
+        private const val ITEM_TYPE_EJERCICIO = 2
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        val inflater = LayoutInflater.from(parent.context)
+        return when (viewType) {
+            ITEM_TYPE_COMIDA -> {
+                val binding = ItemComidasBinding.inflate(inflater, parent, false)
+                ComidaViewHolder(binding)
+            }
+            ITEM_TYPE_MEDIDA -> {
+                val binding = ItemMedidasBinding.inflate(inflater, parent, false)
+                MedidaViewHolder(binding)
+            }
+            ITEM_TYPE_EJERCICIO -> {
+                val binding = ItemEjerciciosBinding.inflate(inflater, parent, false)
+                EjercicioViewHolder(binding)
+            }
+            else -> throw IllegalArgumentException("Invalid view type")
+        }
+    }
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        val item = getItem(position)
+        when (holder) {
+            is ComidaViewHolder -> {
+                val comida = item as Comidas
+                holder.bind(comida)
+            }
+            is MedidaViewHolder -> {
+                val medida = item as Medidas
+                holder.bind(medida)
+            }
+            is EjercicioViewHolder -> {
+                val ejercicio = item as Ejercicios
+                holder.bind(ejercicio)
+            }
+        }
+    }
+
+    override fun getItemCount(): Int {
+        return comidas.size + medidas.size + ejercicios.size
+    }
+
+    override fun getItemViewType(position: Int): Int {
+        val item = getItem(position)
+        return when (item) {
+            is Comidas -> ITEM_TYPE_COMIDA
+            is Medidas -> ITEM_TYPE_MEDIDA
+            is Ejercicios -> ITEM_TYPE_EJERCICIO
+            else -> throw IllegalArgumentException("Invalid item type")
+        }
+    }
+
+    private fun getItem(position: Int): Any {
+        val comidaSize = comidas.size
+        val medidaSize = medidas.size
+
+        return when {
+            position < comidaSize -> comidas[position]
+            position < comidaSize + medidaSize -> medidas[position - comidaSize]
+            else -> ejercicios[position - comidaSize - medidaSize]
+        }
+    }
+
+    inner class ComidaViewHolder(private val binding: ItemComidasBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+
+        init {
+            binding.root.setOnClickListener {
+                val position = adapterPosition
+                if (position != RecyclerView.NO_POSITION) {
+                    val comida = getItem(position) as Comidas
+                    onItemClick(comida)
+                }
+            }
+        }
+
+        fun bind(comida: Comidas) {
+            binding.tvcomida.text = comida.comida
+            binding.tvcalorias.text = "${comida.calorias.toString()} calorias"
+        }
+    }
+
+    inner class MedidaViewHolder(private val binding: ItemMedidasBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+
+        init {
+            binding.root.setOnClickListener {
+                val position = adapterPosition
+                if (position != RecyclerView.NO_POSITION) {
+                    val medida = getItem(position) as Medidas
+                    onItemClick(medida)
+                }
+            }
+        }
+
+        fun bind(medida: Medidas) {
+            binding.tvmusculo.text = medida.musculo
+            binding.tvmedida.text = "${medida.tamaño.toString()} cm"
+        }
+    }
+
+    inner class EjercicioViewHolder(private val binding: ItemEjerciciosBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+
+        init {
+            binding.root.setOnClickListener {
+                val position = adapterPosition
+                if (position != RecyclerView.NO_POSITION) {
+                    val ejercicio = getItem(position) as Ejercicios
+                    onItemClick(ejercicio)
+                }
+            }
+        }
+
+        fun bind(ejercicio: Ejercicios) {
+            binding.tvNombreEjercicio.text = ejercicio.nombre
+            binding.tvCantidadRepeticiones.text = "Repeticiones: ${ejercicio.repeticiones}"
+            binding.tvNumeroSeries.text = "Series: ${ejercicio.series}"
+        }
+    }
+}
--- app/src/main/java/ni/edu/uca/systemfits/ui/view/MenuPrincipal.kt
+++ app/src/main/java/ni/edu/uca/systemfits/ui/view/MenuPrincipal.kt
@@ -5,16 +5,20 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.recyclerview.widget.LinearLayoutManager
+import ni.edu.uca.systemfits.data.database.dao.adapter.MenuPrincipalAdapter
+import ni.edu.uca.systemfits.data.database.entities.Comidas
+import ni.edu.uca.systemfits.data.database.entities.Medidas
+import ni.edu.uca.systemfits.data.database.entities.Ejercicios
 import ni.edu.uca.systemfits.databinding.FragmentMenuPrincipalBinding
+import ni.edu.uca.systemfits.ui.viewmodel.ComidasViewModel
+import ni.edu.uca.systemfits.ui.viewmodel.EjerciciosViewModel
+import ni.edu.uca.systemfits.ui.viewmodel.MedidasViewModel
 
 class MenuPrincipal : Fragment() {
-    private lateinit var binding: FragmentMenuPrincipalBinding
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-        }
-    }
+    private lateinit var binding: FragmentMenuPrincipalBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -22,5 +26,49 @@ class MenuPrincipal : Fragment() {
     ): View? {
         binding = FragmentMenuPrincipalBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val viewModelComidas: ComidasViewModel by viewModels()
+        val viewModelMedidas: MedidasViewModel by viewModels()
+        val viewModelEjercicios: EjerciciosViewModel by viewModels()
+
+        val menuPrincipalAdapter =
+            MenuPrincipalAdapter(emptyList(), emptyList(), emptyList()) { item ->
+                when (item) {
+                    is Comidas -> {
+                        // Lógica para manejar el clic en un item de Comidas
+                    }
+                    is Medidas -> {
+                        // Lógica para manejar el clic en un item de Medidas
+                    }
+                    is Ejercicios -> {
+                        // Lógica para manejar el clic en un item de Ejercicios
+                    }
+                    else -> {
+                        // Lógica para manejar otros tipos de items (si corresponde)
+                    }
+                }
+            }
+        binding.rcvMenu.adapter = menuPrincipalAdapter
+        binding.rcvMenu.layoutManager = LinearLayoutManager(requireContext())
+
+        viewModelComidas.todos.observe(viewLifecycleOwner) { comidas ->
+            val medidas: List<Medidas> = viewModelMedidas.todos.value ?: emptyList()
+            val ejercicios: List<Ejercicios> = viewModelEjercicios.todos.value ?: emptyList()
+            menuPrincipalAdapter.updateData(comidas, medidas, ejercicios)
+        }
+        viewModelMedidas.todos.observe(viewLifecycleOwner) { medidas ->
+            val comidas: List<Comidas> = viewModelComidas.todos.value ?: emptyList()
+            val ejercicios: List<Ejercicios> = viewModelEjercicios.todos.value ?: emptyList()
+            menuPrincipalAdapter.updateData(comidas, medidas, ejercicios)
+        }
+        viewModelEjercicios.todos.observe(viewLifecycleOwner) { ejercicios ->
+            val comidas: List<Comidas> = viewModelComidas.todos.value ?: emptyList()
+            val medidas: List<Medidas> = viewModelMedidas.todos.value ?: emptyList()
+            menuPrincipalAdapter.updateData(comidas, medidas, ejercicios)
+        }
     }
 }
